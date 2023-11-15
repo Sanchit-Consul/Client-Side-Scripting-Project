51bb5900ec5828e355309e1a4ebff8c7817318f9
Sanchit-Consul <89158313+Sanchit-Consul@users.noreply.github.com>
2023-11-15 11:42:43 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var x1Input = document.getElementById('x1').value;
+    var y1Input = document.getElementById('y1').value;
+    var x2Input = document.getElementById('x2').value;
+    var y2Input = document.getElementById('y2').value;
+}
