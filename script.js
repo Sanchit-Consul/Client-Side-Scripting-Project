ba714fd7ac74bdc8ba7d2cbae5ff212f9ee0ad7d
Sanchit-Consul <89158313+Sanchit-Consul@users.noreply.github.com>
2023-11-16 09:34:10 -0700

    continue working on script

--- script.js
+++ script.js
@@ -17,5 +17,36 @@ function calculate() {
         alert('The points cannot be the same. Please enter different points')
     }
 
-    var 
+    var m = (y2 - y1) / (x2 - x1)
+
+    if (!isFinite(m)) {
+        alert('The slope is undefined. PLease enter different ports.')
+    }
+
+    var b = y1 - m * x1;
+
+    var equation = `y = ${m.toFixed(2)}x + ${b.toFixed(2)}`;
+    document.getElementById('result').innerText = equation;
+
+    drawLine([x1, y1], [x2, y2], m, b);
+}
+
+function isValidInput(x1, y1, x2, y2) {
+    return !isNaN(parseFloat(x1)) && !isNaN(parseFloat(y1)) && !isNaN(parseFloat(x2)) && !isNaN(parseFloat(y2));
+}
+
+function drawLine(point1, point2, m, b) {
+    var canvas = document.getElementById('canvas');
+    var ctx = vanvas = vancas.getContext('2d');
+
+    ctx.clearRect (0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.moveTo(point1[0], point1[1])
+    ctx.moveTo(point2[0], point2[1]);
+    ctx.strokeStyle = '#FF0000';
+    ctx.stoke();
+
+    var slopeText = `Slope: ${m.toFixed(2)}`;
+    
 }
