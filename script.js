cf7c110df35f796ae37ec9b17ddc74325667fef4
Sanchit-Consul <89158313+Sanchit-Consul@users.noreply.github.com>
2023-12-11 11:41:05 -0700

    Added more calculator and updated CSS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+// Calculator 1 - Two Points
 function calculate() {
     var x1Input = document.getElementById('x1').value;
     var y1Input = document.getElementById('y1').value;
     var x2Input = document.getElementById('x2').value;
     var y2Input = document.getElementById('y2').value;
 
-    // Convert input values to numbers
     var x1 = parseFloat(x1Input);
     var y1 = parseFloat(y1Input);
     var x2 = parseFloat(x2Input);
@@ -44,44 +44,110 @@ function calculate() {
     }
 
     var equation = `${slopeText} + ${b.toFixed(2)}`;
-    document.getElementById('result').innerText = equation + `\nSlope (m): ${m.toFixed(2)}`;
+    document.getElementById('result').innerText = equation;
 
-    drawLine([x1, y1], [x2, y2], m, b);
+    drawLine([x1, y1], [x2, y2], m, b, 'canvas1');
 }
 
-function drawLine(point1, point2, m, b) {
-    var canvas = document.getElementById('canvas');
+// Calculator 2 - Point and Slope
+function calculateWithSlope() {
+    var xInput = document.getElementById('x').value;
+    var yInput = document.getElementById('y').value;
+    var slopeInput = document.getElementById('slope').value;
+
+    var x = parseFloat(xInput);
+    var y = parseFloat(yInput);
+    var slope = parseFloat(slopeInput);
+
+    if (isNaN(x) || isNaN(y) || isNaN(slope)) {
+        alert('Invalid input. Please enter valid numerical values for the coordinates and slope.');
+        return;
+    }
+
+    var b = y - slope * x;
+
+    var slopeText;
+    if (slope === 1) {
+        slopeText = 'Slope: x';
+    } else if (slope === -1) {
+        slopeText = 'Slope: -x';
+    } else {
+        slopeText = slope > 0 ? `Slope: ${slope.toFixed(2)}` : `Slope: -${Math.abs(slope).toFixed(2)}`;
+    }
+
+    var equation = `${slopeText} + ${b.toFixed(2)}`;
+    document.getElementById('result2').innerText = equation;
+
+    drawLine([x, y], [x + 1, y + slope], slope, b, 'canvas2');
+}
+
+// Calculator 3 - Slope and Y-Intercept
+function calculateWithSlopeAndYIntercept() {
+    var slopeInput = document.getElementById('slope3').value;
+    var yInterceptInput = document.getElementById('yIntercept').value;
+
+    var slope = parseFloat(slopeInput);
+    var yIntercept = parseFloat(yInterceptInput);
+
+    if (isNaN(slope) || isNaN(yIntercept)) {
+        alert('Invalid input. Please enter valid numerical values for slope and y-intercept.');
+        return;
+    }
+
+    var equation = `y = ${slope.toFixed(2)}x + ${yIntercept.toFixed(2)}`;
+    document.getElementById('result3').innerText = equation;
+
+    drawLineWithEquation(slope, yIntercept, 'canvas3');
+}
+
+// Calculator 4 - Parallel Line and Point
+function calculateParallelLine() {
+    var parallelSlopeInput = document.getElementById('parallelSlope').value;
+    var parallelPointInput = document.getElementById('parallelPoint').value;
+
+    var parallelSlope = parseFloat(parallelSlopeInput);
+
+    if (isNaN(parallelSlope)) {
+        alert('Invalid input. Please enter a valid numerical value for the parallel line slope.');
+        return;
+    }
+
+    var parallelPointCoordinates = parallelPointInput.split(',').map(coord => parseFloat(coord.trim()));
+
+    if (parallelPointCoordinates.length !== 2 || isNaN(parallelPointCoordinates[0]) || isNaN(parallelPointCoordinates[1])) {
+        alert('Invalid input. Please enter valid numerical values for the point coordinates in the format "x, y".');
+        return;
+    }
+
+    var xParallel = parallelPointCoordinates[0];
+    var yParallel = parallelPointCoordinates[1];
+
+    var perpendicularSlope = -1 / parallelSlope;
+
+    var equation = `y = ${perpendicularSlope.toFixed(2)}x + ${yParallel - perpendicularSlope * xParallel}`;
+    document.getElementById('result4').innerText = equation;
+
+    drawLine([xParallel - 10, yParallel - 10 * perpendicularSlope], [xParallel + 10, yParallel + 10 * perpendicularSlope], perpendicularSlope, yParallel - perpendicularSlope * xParallel, 'canvas4');
+}
+
+// Function to draw a line based on slope and y-intercept
+function drawLineWithEquation(slope, yIntercept, canvasId) {
+    var canvas = document.getElementById(canvasId);
     var ctx = canvas.getContext('2d');
 
-    // Clear previous drawings
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw x-axis
     ctx.beginPath();
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(canvas.width, canvas.height / 2);
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
+    ctx.moveTo(-canvas.width / 2, canvas.height / 2 - slope * (-canvas.width / 2) - yIntercept);
 
-    // Draw y-axis
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, canvas.height);
-    ctx.stroke();
-
-    // Draw the line
-    ctx.beginPath();
-
-    // Determine the intersection points of the line with the canvas edges
     var xMin = -canvas.width / 2;
     var xMax = canvas.width / 2;
-    var yMin = m * xMin + b;
-    var yMax = m * xMax + b;
+    var yMin = slope * xMin + yIntercept;
+    var yMax = slope * xMax + yIntercept;
 
-    // Adjust the coordinates to fit within the canvas
     xMin += canvas.width / 2;
     xMax += canvas.width / 2;
-    yMin = canvas.height / 2 - yMin * 10; // Scale factor (10) is for better visualization
+    yMin = canvas.height / 2 - yMin * 10;
     yMax = canvas.height / 2 - yMax * 10;
 
     ctx.moveTo(xMin, yMin);
@@ -89,13 +155,24 @@ function drawLine(point1, point2, m, b) {
     ctx.strokeStyle = '#FF0000';
     ctx.stroke();
 
-    // Draw slope text
-    var slopeText = `Slope: ${m.toFixed(2)}`;
+    var slopeText;
+    if (slope === 1) {
+        slopeText = 'Slope: x';
+    } else if (slope === -1) {
+        slopeText = 'Slope: -x';
+    } else {
+        slopeText = slope > 0 ? `Slope: ${slope.toFixed(2)}` : `Slope: -${Math.abs(slope).toFixed(2)}`;
+    }
+
+    var yInterceptText = `y-Intercept: ${yIntercept.toFixed(2)}`;
     ctx.font = '14px Arial';
     ctx.fillStyle = '#000000';
     ctx.fillText(slopeText, 10, 20);
-
-    // Draw y-intercept text
-    var yInterceptText = `y-Intercept: ${b.toFixed(2)}`;
     ctx.fillText(yInterceptText, 10, 40);
 }
+
+// Example usage with dummy data
+calculate();
+calculateWithSlope();
+calculateWithSlopeAndYIntercept();
+calculateParallelLine();
